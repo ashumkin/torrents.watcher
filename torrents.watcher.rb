34ac9aeb0756eea33471cf85eba8d704a64630bb
Alexey Shumkin <Alex.Crezoff@gmail.com>
2012-11-29 02:35:23 +0400

    first commit
    kinokopilka.tv, rutracker.org plugins

---
+++ torrents.watcher.rb
@@ -0,0 +1,533 @@
+#!/usr/bin/env ruby
+
+=begin
+=end
+
+require 'optparse'
+require 'ostruct'
+require 'pp'
+require 'logger'
+require 'fileutils'
+
+module TorrentsWatcher
+
+class ChDir
+	def initialize(dir)
+		return unless block_given?
+		od = Dir.pwd
+		begin
+			Dir.chdir(dir)
+			yield dir
+		ensure
+			Dir.chdir(od)
+		end
+	end
+end
+
+class TOptions < OpenStruct
+	def files
+		# remove trailing path separator
+		return Dir[plugins.gsub(/\/$/, '') + '/' + plugins_mask]
+	end
+end
+
+class TCmdLineOptions < OptionParser
+	attr_reader :options
+
+	def initialize(args)
+		super()
+		@options = TOptions.new
+		@options.config_dir = ENV['HOME'] + '/.torrents.watcher'
+		@options.config = @options.config_dir + '/.configrc'
+		@options.plugins = File.dirname(__FILE__) + '/trackers.d/'
+		@options.plugins_mask = '*.tracker'
+		@options.level = Logger::INFO
+		@options.relogin = false
+		@options.cache = @options.config_dir + '/cache'
+		@options.sync = false
+		@options.dry_run = false
+		init
+		parse!(args)
+		validate
+	end
+
+	def cache
+		return @options.cache
+	end
+
+private
+	def init
+		separator ''
+		separator 'Options:'
+
+		on('-C', '--clean',
+				'Clean up cache (remove *.torrent-files)') do |c|
+			@options.cleanup = true
+		end
+
+		on('-c', '--config CONFIG',
+				'Use configuration file CONFIG instead of ~/.torrents.watcher.rc') do |c|
+			@options.config = c
+		end
+
+		on('-d', '--debug', 'Debug mode. Print all messages') do
+			@options.level = Logger::DEBUG
+		end
+
+		on('-L', '--relogin', 'Relogin (clean cookies)') do
+			@options.relogin = true
+		end
+
+		on('-n', '--dry-run', 'Dry run. Do not copy files (implies --sync)') do
+			@options.dry_run = true
+		end
+
+		on('-s', '--sync FOLDER', 'Sync with transmission watch folder') do |f|
+			@options.sync_folder = f
+			@options.sync = true
+		end
+
+		on('-q', '--quiet', 'Quiet mode') do
+			@options.level = Logger::ERROR
+		end
+
+		on_tail('-h', '--help', 'Show this help') do
+			puts self
+			exit
+		end
+	end
+
+	def validate
+	end
+end
+
+class Tracker
+	attr_reader :name
+
+	def initialize(owner, config)
+		@owner = owner
+		@enabled = true
+		@valid, @hash = self.class.read_config_file(config)
+		unless @valid
+			log(Logger::WARN, "WARNING! File #{config} is not a valid tracker description!")
+			return
+		end
+		@name = @hash.keys[0]
+		# only the one configuration only per file
+		@hash = @hash[@name]
+		# enabled if enabled both in config and in plugin
+		@enabled = self.class.test_enabled(@hash[:enabled])
+		@enabled &&= self.class.test_enabled(logins[:enabled]) if logins
+		@login_method = nil
+	end
+
+	def self.test_enabled(enabled)
+		unless enabled.nil?
+			if enabled.kind_of?(Integer)
+				return enabled != 0
+			else
+				return enabled
+			end
+		end
+		return true
+	end
+
+	def self.read_config_file(file)
+		content = ''
+		return nil, {} unless File.exists?(file)
+		File.open(file, 'r') do |f|
+			content = f.read
+		end
+		begin
+			hash = eval(content)
+		rescue SyntaxError => se
+			hash = {}
+		end
+		if hash.kind_of?(Hash) && !hash.keys[0].nil?
+			return true, hash
+		else
+			return false, {}
+		end
+	end
+
+	def login_method
+		# use caching
+		return @login_method if @login_method
+		if @hash[:login].kind_of?(Symbol)
+			log(Logger::DEBUG, 'Using ' + @hash[:login].to_s + ' login details')
+			@login_method = @owner.find_tracker(@hash[:login]).login_method
+		else
+			@login_method = @hash[:login]
+		end
+		return @login_method
+	end
+
+private
+	def name_with_subst
+		if @hash[:login].kind_of?(Symbol)
+			return @hash[:login]
+		else
+			return @name
+		end
+	end
+
+	def logins
+		name = name_with_subst
+		return nil unless @owner.logins[name]
+		r = @owner.logins[name].dup
+		# use own avalability but referenced
+		r[:enabled] = @owner.logins[@name][:enabled]
+		return r
+	end
+
+	def tmp
+		dir = @owner.opts.cache
+		FileUtils.mkdir_p(dir)
+		return dir
+	end
+
+	def cookies
+		name = name_with_subst
+		return "#{tmp}/#{name}.cookies"
+	end
+
+	def check_login_url
+		return login_method[:check] || login_url
+	end
+
+	def login_url
+		return login_method[:form]
+	end
+
+	def temp_html
+		return "#{tmp}/#{@name}.html"
+	end
+
+	def headers
+		return "#{tmp}/#{@name}.headers"
+	end
+
+	def wget_options
+		opts = ['-q']
+		opts << '--convert-links'
+		opts << '--keep-session-cookies'
+		opts << "--save-cookies #{cookies}"
+		opts << "--load-cookies #{cookies}"
+		opts << ['--server-response', "--output-file #{headers}"]
+		opts.join(' ')
+	end
+
+	def login_data
+		fields = login_method[:fields]
+		data = ''
+		fields.each do |k, v|
+			data << '&'
+			if v.kind_of?(::Symbol)
+				# if config for tracker is set
+				if logins
+					# set param name as symbol
+					k = v
+					# set value as value of param from config
+					v = logins[v].to_s
+				else
+					v = ''
+				end
+			end
+			data << "#{k.to_s}=#{v}"
+		end
+		return "\"#{data}\""
+	end
+
+	def run_wget(url, data = '', tofile = true)
+		file = "-O #{temp_html}" if tofile
+		cmd = "wget #{file.to_s} #{wget_options} #{url}"
+		data = data.join(' ') if data.kind_of?(Array)
+		cmd << ' ' << data.to_s
+		log(Logger::DEBUG, cmd)
+		system(cmd)
+		r = $? == 0
+		log(Logger::ERROR, "Error wget execution") unless r
+		return r
+	end
+
+	def check_already_logged_in
+		return false unless run_wget(check_login_url)
+		return check_login
+	end
+
+	def login
+		# check already logged in?
+		return true if check_already_logged_in
+		# no? try to log in
+		return false unless run_wget(login_url, ['--post-data', login_data])
+		# check
+		return check_login
+	end
+
+	def do_replace_torrent(url, regexp)
+		match_re = @hash[:torrent][:match_re]
+		replace = @hash[:torrent][:replace]
+		# return hash to use source link as a referer
+		return { url.gsub(match_re, replace) => url }
+	end
+
+	def do_scan_torrent(url, regexp)
+		match_re = @hash[:torrent][:match_re]
+		match_index = @hash[:torrent][:match_index]
+		match_index ||= 0
+		a = {}
+		File.open(temp_html) do |f|
+			while line = f.gets
+				line = convert_line(line) if @charset
+				if m = match_re.match(line)
+					link = m[match_index]
+					log(Logger::DEBUG, "Found #{link} (#{m[0]})")
+					if regexp.match(line)
+						log(Logger::DEBUG, 'Matched to ' + regexp.to_s)
+						a[link] = url
+					else
+						log(Logger::DEBUG, 'But not matched to ' + regexp.to_s)
+					end
+				end
+			end
+		end
+		return a
+	end
+
+	def scan_torrent(url, regexp)
+		return unless @hash[:torrent]
+		if @hash[:torrent][:replace_url]
+			return do_replace_torrent(url, regexp)
+		else
+			return do_scan_torrent(url, regexp)
+		end
+	end
+
+	def get_downloaded_filename
+		File.open(headers, 'r') do |f|
+			while line = f.gets
+				if m = /Content-Disposition: attachment; filename="(.+)"/i.match(line)
+					log(Logger::DEBUG, 'Filename is ' + m[1])
+					return m[1]
+				end
+			end
+		end
+	end
+
+	def fetch_urls
+		post = @hash[:torrent][:post]
+		torrents = @hash[:torrent][:url]
+		torrents = logins[:torrents] unless torrents
+		torrents = [torrents] if torrents.kind_of?(String)
+		links = {}
+		params = ['--content-disposition', '-N']
+		if torrents.kind_of?(Array)
+			ts = {}
+			torrents.each do |t|
+				ts[t] = //
+			end
+			torrents = ts
+		end
+		torrents.each do |t, re|
+			log(Logger::INFO, "Checking #{t}")
+			if run_wget(t)
+				links.merge!(scan_torrent(t, re))
+			end
+		end
+		params << '--post-data ""' if post
+		ChDir.new(tmp) do
+			links.each do |link, ref|
+				log(Logger::INFO, 'Fetching ' + link)
+				run_wget(link, params)
+				filename = get_downloaded_filename
+				if filename
+					log(Logger::DEBUG, "Moving #{temp_html} -> #{filename}")
+					FileUtils.mv(temp_html, filename)
+				end
+			end
+		end
+	end
+
+	def scanhtml4charset
+		File.open(temp_html, 'r') do |f|
+			while line = f.gets
+				begin
+					if m = /content=('|")text\/html;\s*charset=(\S+)\1/i.match(line)
+						charset = m[2]
+						return charset
+					end
+				rescue
+					next
+				end
+			end
+		end
+		return nil
+	end
+
+	def convert_line(line)
+		if /^1.9/.match(RUBY_VERSION)
+			line.encode!('UTF-8', @charset)
+		else
+			# Iconv must already be loaded in check_login
+			line = Iconv.iconv('UTF-8', @charset, line)[0]
+		end
+	end
+
+	def check_login
+		r = true
+		success_re = login_method[:success_re] if login_method
+		# iconv is deprecated in Ruby 1.9.x
+		require 'iconv' if (@charset = scanhtml4charset) && !/^1.9/.match(RUBY_VERSION)
+		r = File.size(temp_html) == 0 if File.exists?(temp_html)
+		File.open(temp_html, 'r') do |f|
+			while line = f.gets
+				line = convert_line(line) if @charset
+				if success_re.match(line)
+					r = true
+					break
+				end
+			end
+		end
+		# invert flag and check
+		r = !r
+		if r
+			log(Logger::INFO, "Logged in successfully")
+		else
+			log(Logger::INFO, "NOT logged in")
+		end
+		return r
+	end
+
+	def log(severity, msg)
+		@owner.log(severity, msg)
+	end
+
+	def cleanup
+		log(Logger::INFO, 'Cleanup for ' + @name.to_s)
+		File.unlink(cookies) if File.exists?(cookies)
+	end
+public
+	def run
+		cleanup if @owner.opts.options.relogin
+		return unless @valid && @enabled
+		log(Logger::INFO, "Tracker #{name} is being checked")
+		return unless login
+		fetch_urls
+	end
+end
+
+class TrackersList < ::Array
+	attr_reader :opts, :logins
+
+	def initialize(owner, opts)
+		super()
+		@owner = owner
+		@opts = opts
+		FileUtils.mkdir_p(@opts.options.config_dir)
+		file = @opts.options.config
+		@valid, @logins = Tracker.read_config_file(file)
+		unless @valid
+			if @valid.nil?
+				log(Logger::ERROR, "Config #{file} is absent")
+			else
+				log(Logger::WARN, "WARNING! Config #{file} is not valid")
+			end
+		end
+		read
+	end
+
+	def log(severity, msg)
+		@owner.log(severity, msg)
+	end
+
+	def run
+		return unless @valid
+		self.each do |tracker|
+			tracker.run
+		end
+	end
+
+	def find_tracker(name)
+		self.each do |t|
+			return t if t.name === name
+		end
+	end
+
+private
+	def read
+		@opts.options.files.each do |file|
+			self << Tracker.new(self, file)
+		end
+	end
+end
+
+class TWatcher
+	def initialize(opts)
+		@opts = opts
+		@logger = Logger.new(STDOUT)
+		@logger.level = @opts.options.level
+		@trackers = TrackersList.new(self, @opts)
+	end
+
+	def log(severity, msg)
+		@logger.log(severity, msg)
+	end
+
+	def run
+		if @opts.options.cleanup
+			cleanup
+		elsif @opts.options.sync
+			sync(@opts.options.sync_folder)
+		else
+			@trackers.run
+		end
+	end
+
+private
+	def cleanup
+		Dir["#{@opts.cache}/*.torrent"].sort.each do |t|
+			log(Logger::INFO, "Removing #{t}")
+			if @opts.options.dry_run
+				log(Logger::INFO, "Dry run")
+			else
+				File.unlink(t)
+			end
+		end
+	end
+
+	def sync(folder)
+		unless File.exists?(folder)
+			log(Logger::ERROR, "Folder #{folder} DOES NOT exist!")
+			return false
+		end
+		Dir["#{@opts.cache}/*.torrent"].sort.each do |t|
+			file = "#{folder}/#{File.basename(t)}.loaded"
+			copy = ! File.exists?(file)
+			if ! copy
+				log(Logger::DEBUG, "File #{file} exists")
+				copy = File.size(file) != File.size(t)
+				log(Logger::DEBUG, "But size match") unless copy
+			else
+				log(Logger::DEBUG, "File #{file} DOES NOT exist")
+			end
+			if copy
+				file = "#{folder}/#{File.basename(t)}"
+				log(Logger::INFO, "Copy #{t} -> #{file}")
+				if @opts.options.dry_run
+					log(Logger::INFO, "Dry run")
+				else
+					FileUtils.cp(t, file)
+				end
+			end
+		end
+	end
+end
+
+end
+
+# if run directly
+if __FILE__ == $0
+	cmdLine = TorrentsWatcher::TCmdLineOptions.new(ARGV)
+	watcher = TorrentsWatcher::TWatcher.new(cmdLine)
+	watcher.run
+end
